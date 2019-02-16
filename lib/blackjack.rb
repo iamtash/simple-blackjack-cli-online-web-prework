149dabb08d7072136b467e95a9276c884e0c8fc2
Natasha Chernis <natashachernis@gmail.com>
2019-02-16 03:55:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,7 @@ def runner
   welcome
   card_sum = initial_round
   until card_sum > 21
-    card_sum = hit(card_sum)
+    card_sum = hit?(card_sum)
     display_card_total(card_sum)
   end
 end_game(card_sum)
